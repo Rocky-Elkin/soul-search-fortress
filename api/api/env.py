308a98ae8878ae153a0572c698ca8b4b64322e3a
Rocky-Elkin <air0cky18@gmail.com>
2026-02-09 14:32:23 -0800

    Add HTTP handler to expose environment variables

---
+++ api/api/env.py
@@ -0,0 +1,21 @@
+from http.server import BaseHTTPRequestHandler
+import json
+import os
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-Type', 'application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.end_headers()
+
+        info = {
+            "google_key_exists": "GOOGLE_API_KEY" in os.environ,
+            "google_key_value_preview": os.environ.get("GOOGLE_API_KEY", "NOT_SET")[:12] + "..." if os.environ.get("GOOGLE_API_KEY") else "NOT_SET",
+            "gemini_key_exists": "GEMINI_API_KEY" in os.environ,  # just to be sure
+            "all_env_keys_sample": list(os.environ.keys())[:25],  # first 25 keys only
+            "vercel_env": os.environ.get("VERCEL_ENV", "not_set"),
+            "python_version": os.environ.get("PYTHON_VERSION", "not_set")
+        }
+
+        self.wfile.write(json.dumps(info, indent=2).encode('utf-8'))
