84f2de9b129cbced49762e21b3f057352c74dd9d
Rocky-Elkin <air0cky18@gmail.com>
2026-02-09 14:47:05 -0800

    Refactor environment info structure in env.py

--- api/api/env.py
+++ api/api/env.py
@@ -9,12 +9,13 @@ class handler(BaseHTTPRequestHandler):
         self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
 
-        env_info = {
+        info = {
             "google_key_exists": "GOOGLE_API_KEY" in os.environ,
             "google_key_preview": os.environ.get("GOOGLE_API_KEY", "NOT_SET")[:12] + "..." if os.environ.get("GOOGLE_API_KEY") else "NOT_SET",
-            "all_env_keys": list(os.environ.keys())[:30],   # first 30 keys only
+            "gemini_key_exists": "GEMINI_API_KEY" in os.environ,
+            "all_env_keys": list(os.environ.keys())[:30],
             "vercel_env": os.environ.get("VERCEL_ENV", "not_set"),
             "python_version": os.environ.get("PYTHON_VERSION", "not_set")
         }
 
-        self.wfile.write(json.dumps(env_info, indent=2).encode('utf-8'))
+        self.wfile.write(json.dumps(info, indent=2).encode('utf-8'))
